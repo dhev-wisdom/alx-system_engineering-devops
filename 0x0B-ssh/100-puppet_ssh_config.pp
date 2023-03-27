0264ca3788db086519863d16cd1a820181de6ef3
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-27 08:29:08 -0700

    Fixing Puppet config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,9 +2,9 @@
 
 file { '/etc/ssh/ssh_config':
     ensure => 'file',
-    owner  => 10.251.0.2,
-    group  => 10.251.0.2,
-    mode   => '0600'
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode => '0600',
 }
 
 file_line { 'set-ssh-config':
