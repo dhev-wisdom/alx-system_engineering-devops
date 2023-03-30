72d0922aa66bd6eb4ea933e0e7be7d3c7b853ba1
dhev-wisdom <nonsowisdom62@gmail.com>
2023-03-30 09:40:11 -0700

    All Good

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # This file creates a simple '/tmp/school' file with Puppet
 
 file { '/tmp/school':
-    ensure => present,
-    content => "I love Puppet",
-    group => "www-data",
-    owner => "www-data",
-    mode => "0744",
+    ensure  => present,
+    content => 'I love Puppet',
+    group   => 'www-data',
+    owner   => 'www-data"'
+    mode    => '0744',
 }
