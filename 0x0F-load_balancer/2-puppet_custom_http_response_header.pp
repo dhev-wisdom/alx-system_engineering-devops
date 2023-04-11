897dd3c0fb1e1fb3c6746e29596b7227c65f4758
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-10 17:32:48 -0700

    Nginx custom header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,14 @@
+# Puppet script to install and set up Nginx with custom header
+
+exec { 'update':
+	provider => shell,
+	command  => 'sudo apt-get update -y',
+}
+
+package { 'nginx':
+	ensure => 'installed',
+}
+
 class { 'nginx':
 	header => {
 		'X-Served-By' => '$host',
@@ -13,4 +24,9 @@ nginx::resource::server {'default':
 			index => 'index.html index.htm',
 		},
 	},
+}
+
+file { '/var/www/html/index.html':
+  content => '<html><body><h1>Holberton School!</h1></body></html>',
+  ensure => 'file',
 }
