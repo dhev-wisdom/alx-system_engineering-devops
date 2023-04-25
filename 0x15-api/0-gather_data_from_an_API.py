fb1aa8239876f6215bc22287b407c81895ecd65b
dhev-wisdom <nonsowisdom62@gmail.com>
2023-04-25 21:00:25 +0100

    api intro with python

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,8 +8,10 @@ import sys
 if __name__ == "__main__":
     id_ = sys.argv[1]
 
-    users = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(id_))
-    todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(id_))
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(id_))
+    todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                         .format(id_))
     data1 = users.json()
     data2 = todos.json()
     completed_tasks = 0
@@ -17,10 +19,11 @@ if __name__ == "__main__":
     total_tasks = 0
     employee_name = data1["name"]
     for i in range(len(data2)):
-        if data2[i]["completed"] == False:
+        if data2[i]["completed"] is False:
             completed_tasks += 1
             complete_tasks.append(data2[i]["title"])
         total_tasks += 1
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
     for i in complete_tasks:
         print("\t", i)
