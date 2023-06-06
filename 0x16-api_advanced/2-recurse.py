aefb514433af38b888439a000f6d47f101260694
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-06 12:45:53 +0100

    Check: Pycodestyle fixed

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,24 +10,21 @@ def recurse(subreddit, hot_list=[], after=None):
     """
     Function to that queries Reddit API and returns list of hot post titles
     in several pages
-    """ 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64i)"}
-    params = {"limit": 10}
-    if after:
-        params["after"] = after
+    """
+    url = ("https://www.reddit.com/r/{}/hot.json"
+           "?after={}".format(subreddit, after))
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64)"}
 
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        results = response.json().get("data")
-        children = results.get("children")
-        if not children:
+        resp_json = response.json()
+        for children in resp_json.get("data").get("children"):
+            hot_list.append(children.get("data").get("title"))
+        after = resp_json.get("data").get("after")
+        if not after:
             return hot_list
-        for child in children:
-            hot_list.append(child.get("data").get("title"))
-        return recurse(subreddit, hot_list=[], results.get("after"))
+        return recurse(subreddit, hot_list, after)
 
     else:
         return None
