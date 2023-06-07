f5c5a5fd329be16551b863e9479192813949287e
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-07 02:07:13 +0100

    Cheeck out

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 import requests
 
 
-def count_words(subreddit, word_list, after=None, word_count=defaultdict(int)):
+def count_words(subreddit, word_list, after=None, word_count=defaultdict(int):
     """
     Function to that queries Reddit API and returns list of hot post titles
     in several pages
@@ -27,22 +27,18 @@ def count_words(subreddit, word_list, after=None, word_count=defaultdict(int)):
         for children in resp_json.get("data").get("children"):
             title = children.get("data").get("title")
             words = title.split()
-
             for word in words:
                 word_lower = word.lower()
                 if word_lower in word_list:
                     word_count[word_lower] += 1
 
         after = resp_json.get("data").get("after")
-
         if not after:
-            _dict = dict(sorted(word_count.items(),
-                         key=lambda x: (-x[1], x[0].lower())))
-            sorted_dict = sorted(_dict.items())
-            for word, count in sorted_dict:
+            sorted_counts = sorted(word_count.items(),
+                                   key=lambda x: x[0].lower())
+            for word, count in sorted_counts:
                 print(f"{word.lower()}: {count}")
             return
         return count_words(subreddit, word_list, after, word_count)
 
-    else:
-        return None
+    return None
