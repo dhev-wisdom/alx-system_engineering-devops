6b18cd2a06e364190896e71ac4ae818168c8dcb3
dhev-wisdom <nonsowisdom62@gmail.com>
2023-06-18 04:28:55 +0100

    Check Puppet

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+#  Increase the amount of traffic the Nginx server can handle.
+
+exec { 'fix-for-nginx':
+	command => 'sed -i "s/15/4096/" /etc/default/nginx',
+	path    => '/usr/local/bin/:/bin/',
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+	command => 'nginx restart',
+	path    => '/etc/init.d/',
+}
